406000ad88dcbbc15ac5d50067246f950f93d611
aa041 <aa041@hdm-stuttgart.de>
2026-05-15 18:54:53 +0200

    onebox finished?

--- plugin.rb
+++ plugin.rb
@@ -1,10 +1,13 @@
 # name: aliexpress-image
 # about: Generates a product image preview for AliExpress links using Admin settings
-# version: 0.2
+# version: 0.3
 # authors: YourName
 # url: https://github.com/yourusername/aliexpress-image
 
 enabled_site_setting :aliexpress_image_enabled
+
+# Register the custom CSS file
+register_asset "stylesheets/aliexpress.scss"
 
 after_initialize do
   require 'digest'
@@ -13,12 +16,10 @@ after_initialize do
 
   module ::AliExpressImage
     class Processor
-      def self.get_img_url(product_id)
-        # Pull values from the Admin Settings
+      def self.get_product_details(product_id)
         app_key = SiteSetting.aliexpress_app_key
         app_secret = SiteSetting.aliexpress_app_secret
 
-        # Safety check: if settings are empty, don't run
         return nil if app_key.blank? || app_secret.blank?
 
         params = {
@@ -33,19 +34,27 @@ after_initialize do
           "target_language" => "EN"
         }
 
-        # 1. Sort and Sign
         sorted_params = params.sort.map { |k, v| "#{k}#{v}" }.join
         signature = OpenSSL::HMAC.hexdigest('SHA256', app_secret, sorted_params).upcase
 
-        # 2. Call API
         uri = URI("https://api-sg.aliexpress.com/sync")
         uri.query = URI.encode_www_form(params.merge("sign" => signature))
         
         response = Net::HTTP.get(uri)
         json = JSON.parse(response)
         
-        # 3. Extract Image
-        json.dig("aliexpress_affiliate_productdetail_get_response", "resp_result", "result", "products", "product", 0, "product_main_image_url")
+        # Extract the entire product object
+        product = json.dig("aliexpress_affiliate_productdetail_get_response", "resp_result", "result", "products", "product", 0)
+        
+        return nil unless product
+
+        # Return a hash of details
+        {
+          title: product["product_title"],
+          image: product["product_main_image_url"],
+          price: product["target_sale_price"],
+          currency: product["target_currency"]
+        }
       rescue => e
         Rails.logger.error("AliExpress Image Plugin Error: #{e.message}")
         nil
@@ -56,13 +65,28 @@ after_initialize do
   on(:post_created) do |post|
     return unless SiteSetting.aliexpress_image_enabled
     
-    # Matches IDs in URLs like /item/12345.html
     ids = post.raw.scan(/aliexpress\.com\/item\/(\d+)\.html/).flatten.uniq
     
     if ids.any?
-      image_url = AliExpressImage::Processor.get_img_url(ids.first)
-      if image_url
-        new_raw = post.raw + "\n\n![AliExpress Product|600x600](#{image_url})"
+      product_id = ids.first
+      details = AliExpressImage::Processor.get_product_details(product_id)
+      
+      if details
+        target_url = "https://www.aliexpress.com/item/#{product_id}.html"
+        
+        # Build the Onebox-style Markdown using the [wrap] syntax
+        card_markdown = <<~MD
+          
+          [wrap=aliexpress-card]
+          [![#{details[:title]}|120x120](#{details[:image]})](#{target_url})
+          [wrap=aliexpress-info]
+          **[#{details[:title]}](#{target_url})**
+          <span class="price">#{details[:price]} #{details[:currency]}</span>
+          [/wrap]
+          [/wrap]
+        MD
+        
+        new_raw = post.raw + "\n" + card_markdown
         post.update_columns(raw: new_raw)
         post.publish_change_to_clients! :cooked
       end
